3089e5c6a963b93b103b5a96766d51a8adae113c
Bemin Shaker <bshaker1@stevens.edu>
2025-04-14 01:20:36 -0400

    new client

---
+++ client-2/src/components/CourseReviews.js
@@ -0,0 +1,72 @@
+import { React, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import "./CourseReviews.css";
+import Spinner from "./Spinner";
+
+function CourseReviews() {
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id, catId, courseId } = useParams();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const apiUrl = process.env.NODE_ENV === 'production'
+    ? `https://review-my-course.vercel.app/api/${id}/${catId}/${courseId}`
+    : `http://localhost:3000/api/${id}/${catId}/${courseId}`;
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setReviews(data);
+        setTimeout(function () {
+          setLoading(false);
+        }, 2000);
+      });
+  };
+
+  if (loading) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="courseReviews">
+          <h1 className="listTitle">
+            <strong>{courseId} </strong> in the <strong>{catId}</strong>{" "}
+            department at <strong>{id}</strong>
+          </h1>
+          <h4 className="rating-title">Average Rating:</h4>
+          <h3 style={{ display: "flex" }}>
+            <span className="rating-big">2.9</span>/ 5
+          </h3>
+          <button className="rate-btn"> Rate {courseId}</button>
+          {reviews &&
+            reviews.length > 0 &&
+            reviews.map((post) => {
+              return (
+                <div className="review-box" key={post.body}>
+                  <h3>{post.body}</h3>
+                  <p>
+                    Professor: <strong>{post.professorName}</strong>
+                  </p>
+                  <p>
+                    Semester Taken: <strong>{post.semesterTaken}</strong>
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CourseReviews;
---
+++ client-2/src/components/Schools.jsx
@@ -0,0 +1,67 @@
+import { React, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import "./Schools.css";
+import Spinner from "./Spinner";
+
+function Schools() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const apiUrl = process.env.NODE_ENV === 'production'
+      ? "https://review-my-course.vercel.app/api"
+      : "http://localhost:3000/api/";
+
+    fetch(apiUrl)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data);
+        setCourses(data);
+        setTimeout(function () {
+          setLoading(false);
+        }, 2000);
+      })
+  }
+
+  console.log(courses);
+
+  if (loading) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="schools">
+          <h1 className="listTitle">Choose one of the available schools</h1>
+          <div className="categoryComponent">
+          {courses &&
+              courses.length > 0 && courses !== undefined &&
+              courses.map((school) => {
+                return (
+                  <Link id="link" key={school.id} to={`/schools/${school.schoolId}`}>
+                    <div className="categoryBox" key={school.id}>
+                      <div>
+                        <h1 >{school.schoolName}</h1>
+                      </div>
+                    </div>  
+                  </Link>
+                );
+              })}
+          </div>
+       </div>
+      </div>
+    )
+  }
+}
+
+
+export default Schools;
